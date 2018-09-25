0e95f3ad62859eaf36d34b3887917e6b76602e8c
fudini <fudimi@gmail.com>
2018-09-25 11:11:50 +0100

    user class for getters / setters

--- src/generators/wrap.ts
+++ src/generators/wrap.ts
@@ -3,34 +3,46 @@ import { genReadFields } from './read'
 
 const genWrap = (obj, write, path = []) => {
 
-  return _.map(obj, (v, k) => {
+  let construct = []
+
+  let content = _.map(obj, (v, k) => {
 
     if (_.isArray(v)) {
       return [
         `set ${k}(v) {\n`,
         `throw 'no support'\n`,
-        `},\n`,
+        `}\n`,
         `get ${k}() {\n`,
         `throw 'no support'\n`,
-        `},\n`
+        `}\n`
       ].join('')
     }
 
     if (_.isObject(v)) {
-      const gend = genWrap(v, write, [...path, k]).join('')
-      return [
-        //`set ${k}(v) { for (let k in v) { this.${k}[k] = v[k] } },\n`,
-        `${k}: {\n`,
-        `${gend}\n`,
-        `},\n`
-      ].join('')
+      const [content, constructInner] = genWrap(v, write, [...path, k])
+      const gend = content.join('')
+
+      const constructContent = constructInner.length
+        ? `constructor() { ${constructInner} }`
+        : ``
+
+      construct.push([
+        `this.${k} = new (class ${k} {\n`,
+          `${constructContent}\n`,
+          `${gend}\n`,
+        `})\n`
+      ].join(''))
+
+      return ''
     }
 
     let writeStatement = _.get(write, [...path, k])
-    let w = `set ${k}(v) { ${writeStatement} },\n`
-    let r = `get ${k}() { return ${v} },\n`
+    let w = `set ${k}(v) { ${writeStatement} }\n`
+    let r = `get ${k}() { return ${v} }\n`
     return w + r
   })
+
+  return [content, construct]
 }
 
 const genWrapFunction = (readers, writers, lookup, name) => {
@@ -38,12 +50,18 @@ const genWrapFunction = (readers, writers, lookup, name) => {
   let [read] = genReadFields(readers, lookup)(name)
   let [write] = genReadFields(writers, lookup)(name)
 
-  let setBuffer = `setBuffer(b) { buffer = b; return this },\n`
-  let getBuffer = `getBuffer(b) { buffer },\n`
-  let all = genWrap(read, write).join('') + setBuffer + getBuffer
+  let setBuffer = `setBuffer(b) { buffer = b; return this }\n`
+  let getBuffer = `getBuffer(b) { buffer }\n`
+  let [content, construct] = genWrap(read, write)
+    
+  let constructWrap = `constructor() {\n` + 
+    `${construct.join('\n')}\n` +
+    `}\n`
+
+  let all = constructWrap + content.join('') + setBuffer + getBuffer
 
   let body = `var buffer = buf\n` + 
-             `return { ${all} }\n`
+             `return new (class Wrap { ${all} })()\n`
   return new Function('buf', body)
 }
 
