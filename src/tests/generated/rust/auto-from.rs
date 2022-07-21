3e28e4801a356a6cffbe2dd34967fef51555c02d
Piotr Maks <piotr.maks@gmail.com>
2022-07-21 16:17:33 +0200

    Fix auto From
    Resolves #59

--- src/tests/generated/rust/auto-from.rs
+++ src/tests/generated/rust/auto-from.rs
@@ -1,18 +1,18 @@
 impl std::convert::From<foo::Foo> for bar::Foo {
   fn from(value: foo::Foo) -> Self {
     match value {
-      foo::Variant1 => Self::Variant1,
-      foo::Variant2 => Self::Variant2,
-      foo::Variant3 => Self::Variant3,
+      foo::Foo::Variant1 => Self::Variant1,
+      foo::Foo::Variant2 => Self::Variant2,
+      foo::Foo::Variant3 => Self::Variant3,
     }
   }
 }
 impl std::convert::From<bar::Foo> for foo::Foo {
   fn from(value: bar::Foo) -> Self {
     match value {
-      bar::Variant1 => Self::Variant1,
-      bar::Variant2 => Self::Variant2,
-      bar::Variant3 => Self::Variant3,
+      bar::Foo::Variant1 => Self::Variant1,
+      bar::Foo::Variant2 => Self::Variant2,
+      bar::Foo::Variant3 => Self::Variant3,
     }
   }
 }
